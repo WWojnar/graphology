cf6587cfa04205efed75b0fc5b84eee7b85e5316
Marek Sośnicki <209191@student.pwr.edu.pl>
2018-01-05 15:10:59 +0100

    Zones analyzer with test v2

--- test_zonesAnalyzer.py
+++ test_zonesAnalyzer.py
@@ -3,16 +3,16 @@ from zonesAnalyzer import ZonesAnalyzer
 class TestZonesAnalyzer:
 
     def test_ZonesEqual(self):
-        assert ZonesAnalyzer.analyze([1, 2, 3, 4]) == "balanced"
+        assert ZonesAnalyzer.analyze([1, 2, 3, 4])["zoneDominance"] == "balanced"
 
     def test_UpperZoneDominant(self):
-        assert ZonesAnalyzer.analyze([20, 60, 80, 110]) == "upper zone dominant"
+        assert ZonesAnalyzer.analyze([20, 60, 80, 110])["zoneDominance"] == "upper zone dominant"
 
     def test_MiddleZoneDominant(self):
-        assert ZonesAnalyzer.analyze([10, 20, 40, 55]) == "middle zone dominant"
+        assert ZonesAnalyzer.analyze([10, 20, 40, 55])["zoneDominance"] == "middle zone dominant"
 
     def test_LowerZoneDominant(self):
-        assert ZonesAnalyzer.analyze([10, 20, 35, 55]) == "lower zone dominant"
+        assert ZonesAnalyzer.analyze([10, 20, 35, 55])["zoneDominance"] == "lower zone dominant"
 
     def test_BalancedZones(self):
-        assert ZonesAnalyzer.analyze([10, 20, 31, 38]) == "balanced"
+        assert ZonesAnalyzer.analyze([10, 20, 31, 38])["zoneDominance"] == "balanced"
