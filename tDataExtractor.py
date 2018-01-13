a04d445b099d9789474aba2834cabee501257ccf
Marek Sośnicki <209191@student.pwr.edu.pl>
2018-01-13 17:04:21 +0100

    T analyzer part 6 crossing length

--- tDataExtractor.py
+++ tDataExtractor.py
@@ -14,6 +14,8 @@ class Colors:
 
 
 class TDataExtractor:
+    maxEdgeThickness = 50
+    maxStemThickness = 80
     def __init__(self, img):
         self.img = img
         self.analysis = {}
@@ -24,6 +26,7 @@ class TDataExtractor:
         self.stemCore = []
         self.leftEdgeStem = []
         self.rightEdgeStem = []
+        self.stemThickness = []
 
     def drawPoints(self, points, color):
         for point in points:
@@ -40,97 +43,21 @@ class TDataExtractor:
         self.img = closing
 
     def findTopLabel(self):
-        bestTopEdge = self.findBestTopEdgeCandidatePoints()
-
+        bestTopEdge = self.findBestTopEdgeLeftMostCandidatePoint()
         labelStartPoint = bestTopEdge[0]
-
         self.findLabelCore(labelStartPoint)
+        self.findLabelEdgesAndThickness()
 
-
-        maxThickness = 50
-        for point in self.labelCore:
-            y = point[0]
-            topFoundEdge = (0, 0)
-            rangeStart = np.max([point[1] - maxThickness, 0])
-            for x in range(point[1] - 1, rangeStart, -1):
-                if self.edges[x][y] > 0:
-                    topFoundEdge = (y, x)
-                    self.topEdgeLabel.append(topFoundEdge)
-                    break
-
-
-            rangeEnd = np.min([point[1] + maxThickness, len(self.edges)])
-            bottomFoundEdge = (0, 0)
-            for x in range(point[1], rangeEnd):
-                if self.edges[x][y] > 0:
-                    bottomFoundEdge = (y, x)
-                    self.bottomEdgeLabel.append(bottomFoundEdge)
-                    break
-            if (topFoundEdge != (0, 0) and bottomFoundEdge != (0, 0)):
-                self.labelThickness.append(np.abs(topFoundEdge[1] - bottomFoundEdge[1]))
-
-    def findLabelCore(self, labelStartPoint):
-        self.labelCore = [labelStartPoint]
-        currentPoint = (labelStartPoint[0] + 4, labelStartPoint[1])
-        direction = 0
-        while (currentPoint[0] < len(self.img[0]) - 10):
-            while (currentPoint[0] < len(self.img[0]) - 1 and np.sum(self.img[currentPoint[1]][currentPoint[0]]) > 0):
-                currentPoint = (currentPoint[0] + 1, currentPoint[1])
-                self.labelCore.append(currentPoint)
-
-            checkRange = self.getRangeForDirection(direction)
-            foundPoint = self.findCandidateInVerticalRange(checkRange, currentPoint)
-            if foundPoint == (0, 0) and direction != 0:
-                checkRange = self.getRangeForDirection(-direction)
-                foundPoint = self.findCandidateInVerticalRange(checkRange, currentPoint)
-
-            if foundPoint == (0, 0):
-                break
-            direction = self.determineVerticalDirection(currentPoint, foundPoint)
-            currentPoint = foundPoint
-            self.labelCore.append(currentPoint)
-
-    def findCandidateInVerticalRange(self, checkRange, currentPoint):
-        foundPoint = (0,0)
-        for change in checkRange:
-            newPoint = (currentPoint[0], currentPoint[1] + change)
-            if (newPoint[1] > 0 and newPoint[1] < len(self.img) and np.sum(self.img[newPoint[1]][newPoint[0]]) > 0):
-                foundPoint = newPoint
-                break
-        return foundPoint
-
-    def determineVerticalDirection(self, currentPoint, foundPoint):
-        if foundPoint[1] > currentPoint[1]:
-            direction = 1
-        else:
-            direction = -1
-        return direction
-
-    def getRangeForDirection(self, direction):
-        if direction == 0:
-            checkRange = [-2, 2, -5, 5, -10, 10, -17, 17, -25, 25, -40, 40]
-        else:
-            checkRange = [direction * 2, direction * 5, direction * 10, direction * 17, direction * 25,
-                          direction * 40]
-        return checkRange
-
-    def findBestTopEdgeCandidatePoints(self):
+    # This function will look for the longest horizontal edge, this edge can have a gap if it keeps it trend
+    def findBestTopEdgeLeftMostCandidatePoint(self):
         bestTopEdge = [(0, 0)]
         currentTopEdge = [(0, 0)]
         lastPoint = (0, 0)
         iterationsAfterChangingTopEdge = 0
         for y in range(1, len(self.edges[0])):
-            minBestTopEdge = 1000000
-            maxBestTopEdge = 0
-            for p in bestTopEdge:
-                if (minBestTopEdge > p[1]):
-                    minBestTopEdge = p[1]
-                if (maxBestTopEdge < p[1]):
-                    maxBestTopEdge = p[1]
-
+            maxBestTopEdge, minBestTopEdge = self.findMinAndMaxVertical(bestTopEdge)
             for x in range(1, len(self.edges)):
                 if self.edges[x][y] != 0:
-
                     if (len(currentTopEdge) < len(bestTopEdge) and iterationsAfterChangingTopEdge > 10
                         and (abs(minBestTopEdge - x) < 40 or abs(maxBestTopEdge - x) < 40 or (
                                         x > minBestTopEdge and x < maxBestTopEdge))):
@@ -150,13 +77,189 @@ class TDataExtractor:
             bestTopEdge = currentTopEdge
         return bestTopEdge
 
+    def findLabelEdgesAndThickness(self):
+        for point in self.labelCore:
+            topFoundEdge = self.findAndAddTopEdgePoint(point)
+            bottomFoundEdge = self.findAndAddBottomEdgePoint(point)
+            if (topFoundEdge != (0, 0) and bottomFoundEdge != (0, 0)):
+                self.labelThickness.append(np.abs(topFoundEdge[1] - bottomFoundEdge[1]))
+
+    def findAndAddBottomEdgePoint(self, point):
+        y = point[0]
+        rangeEnd = np.min([point[1] + TDataExtractor.maxEdgeThickness, len(self.edges)])
+        bottomFoundEdge = (0, 0)
+        for x in range(point[1], rangeEnd):
+            if self.edges[x][y] > 0:
+                bottomFoundEdge = (y, x)
+                self.bottomEdgeLabel.append(bottomFoundEdge)
+                break
+        return bottomFoundEdge
+
+    def findAndAddTopEdgePoint(self, point):
+        y = point[0]
+        topFoundEdge = (0, 0)
+        rangeStart = np.max([point[1] - TDataExtractor.maxEdgeThickness, 0])
+        for x in range(point[1] - 1, rangeStart, -1):
+            if self.edges[x][y] > 0:
+                topFoundEdge = (y, x)
+                self.topEdgeLabel.append(topFoundEdge)
+                break
+        return topFoundEdge
+
+    def findLabelCore(self, labelStartPoint):
+        self.labelCore = [labelStartPoint]
+        currentPoint = (labelStartPoint[0] + 4, labelStartPoint[1])
+        direction = 0
+        while (currentPoint[0] < len(self.img[0]) - 10):
+            currentPoint = self.addAsManyPointsAsPossibleToTheRight(currentPoint)
+            foundPoint = self.findCandidatePointAboveOrBelow(currentPoint, direction)
+            if foundPoint == (0, 0):
+                break
+            direction = self.determineVerticalDirection(currentPoint, foundPoint)
+            currentPoint = foundPoint
+            self.labelCore.append(currentPoint)
+
+    def addAsManyPointsAsPossibleToTheRight(self, currentPoint):
+        while (currentPoint[0] < len(self.img[0]) - 1 and np.sum(self.img[currentPoint[1]][currentPoint[0]]) > 0):
+            currentPoint = (currentPoint[0] + 1, currentPoint[1])
+            self.labelCore.append(currentPoint)
+        return currentPoint
+
+    def findCandidatePointAboveOrBelow(self, currentPoint, direction):
+        checkRange = self.getRangeForDirection(direction)
+        foundPoint = self.findCandidateInVerticalRange(checkRange, currentPoint)
+        if foundPoint == (0, 0) and direction != 0:
+            checkRange = self.getRangeForDirection(-direction)
+            foundPoint = self.findCandidateInVerticalRange(checkRange, currentPoint)
+        return foundPoint
+
+    def findCandidateInVerticalRange(self, checkRange, currentPoint):
+        foundPoint = (0,0)
+        for change in checkRange:
+            newPoint = (currentPoint[0], currentPoint[1] + change)
+            if (newPoint[1] > 0 and newPoint[1] < len(self.img) and np.sum(self.img[newPoint[1]][newPoint[0]]) > 0):
+                foundPoint = newPoint
+                break
+        return foundPoint
+
+    # TODO: some helpers?
+    def determineVerticalDirection(self, currentPoint, foundPoint):
+        if foundPoint[1] > currentPoint[1]:
+            direction = 1
+        else:
+            direction = -1
+        return direction
+
+    def getRangeForDirection(self, direction):
+        if direction == 0:
+            checkRange = [-2, 2, -5, 5, -10, 10, -17, 17, -25, 25, -40, 40]
+        else:
+            checkRange = [direction * 2, direction * 5, direction * 10, direction * 17, direction * 25,
+                          direction * 40]
+        return checkRange
+
+
+
+    def findMinAndMaxVertical(self, bestTopEdge):
+        minBestTopEdge = 1000000
+        maxBestTopEdge = 0
+        for p in bestTopEdge:
+            if (minBestTopEdge > p[1]):
+                minBestTopEdge = p[1]
+            if (maxBestTopEdge < p[1]):
+                maxBestTopEdge = p[1]
+        return maxBestTopEdge, minBestTopEdge
+
+    def findStem(self):
+        topPoint = self.findTopPoint()
+        self.findStemCore(topPoint)
+        self.findStemEdgesAndThickness()
+
+    def findTopPoint(self):
+        topPoint = (0, 0)
+        for x in range(0, len(self.img)):
+            for y in range(0, len(self.img[x])):
+                if self.edges[x][y] > 0:
+                    topPoint = (y, x)
+                    break
+            if topPoint != (0, 0):
+                break
+        return topPoint
+
+    def findStemCore(self, topPoint):
+        self.stemCore = [topPoint]
+        currentPoint = (topPoint[0], topPoint[1] + 4)
+        direction = 0
+        while (currentPoint[1] < len(self.img) - 10):
+            while (currentPoint[1] < len(self.img) and np.sum(self.img[currentPoint[1]][currentPoint[0]]) > 0):
+                currentPoint = (currentPoint[0], currentPoint[1] + 1)
+                self.stemCore.append(currentPoint)
+            foundPoint = self.findCandidateInHorizontalRange(self.getRangeForDirection(direction), currentPoint)
+            if foundPoint == (0, 0) and direction != 0:
+                foundPoint = self.findCandidateInHorizontalRange(self.getRangeForDirection(-direction), currentPoint)
+            if foundPoint == (0, 0):
+                break
+            direction = self.determineNewHorizontalDirection(currentPoint, foundPoint)
+            currentPoint = foundPoint
+            self.stemCore.append(currentPoint)
+
+
+    def findCandidateInHorizontalRange(self, checkRange, currentPoint):
+        foundPoint = (0, 0)
+        for change in checkRange:
+            newPoint = (currentPoint[0] + change, currentPoint[1])
+            if (newPoint[0] > 0 and np.sum(self.img[newPoint[1]][newPoint[0]]) > 0):
+                foundPoint = newPoint
+                break
+        return foundPoint
+
+
+    def findStemEdgesAndThickness(self):
+        for point in self.stemCore:
+            leftFoundEdge = self.findAndAddLeftEdgePoint(point)
+            rightFoundEdge = self.findAndAddRightEdgePoint(point)
+            if (rightFoundEdge != (0, 0) and leftFoundEdge != (0, 0)):
+                self.stemThickness.append(np.abs(rightFoundEdge[0] - leftFoundEdge[0]))
+
+    def findAndAddRightEdgePoint(self, point):
+        x = point[1]
+        rangeEnd = np.min([point[0] + TDataExtractor.maxStemThickness, len(self.edges[x])])
+        rightFoundEdge = (0, 0)
+        for y in range(point[0], rangeEnd):
+            if self.edges[x][y] > 0:
+                rightFoundEdge = (y, x)
+                self.rightEdgeStem.append(rightFoundEdge)
+                break
+        return rightFoundEdge
+
+    def findAndAddLeftEdgePoint(self, point):
+        x = point[1]
+        leftFoundEdge = (0, 0)
+        rangeStart = np.max([point[0] - TDataExtractor.maxStemThickness, 0])
+        for y in range(point[0] - 1, rangeStart, -1):
+            if self.edges[x][y] > 0:
+                leftFoundEdge = (y, x)
+                self.leftEdgeStem.append(leftFoundEdge)
+                break
+        return leftFoundEdge
+
+    def determineNewHorizontalDirection(self, currentPoint, foundPoint):
+        if foundPoint[0] > currentPoint[0]:
+            direction = 1
+        else:
+            direction = -1
+        return direction
+
+
+
+
+
     def getLabelThicknessAnalysis(self):
         poly = np.polyfit(range(0, len(self.labelThickness)), self.labelThickness, 1)
         # [ 0.21965908  8.96886355] - rosnaca
         # [ -0.08947244  61.56185738] -malejaca
         # [ -3.54252225e-03   2.54786461e+01] - rowne
         # patrzymy na 1 parametr aby analizowac wzrost - spadek - jak fukncja liniowa
-
 
         if abs(poly[0]) < 0.05:
             return "constant"
@@ -178,101 +281,37 @@ class TDataExtractor:
             else:
                 return "rising"
 
-    def findStem(self):
-        topPoint = (0, 0)
-        for x in range(0, len(self.img)):
-            for y in range(0, len(self.img[x])):
-                if self.edges[x][y] > 0:
-                    topPoint = (y, x)
-                    break
-            if topPoint != (0, 0):
-                break
-
-        self.stemCore = [topPoint]
-
-        currentPoint = (topPoint[0], topPoint[1] + 4)
-
-        direction = 0
-        while (currentPoint[1] < len(self.img) - 10):
-            while (currentPoint[1] < len(self.img) and np.sum(self.img[currentPoint[1]][currentPoint[0]]) > 0):
-                currentPoint = (currentPoint[0], currentPoint[1] + 1)
-                self.stemCore.append(currentPoint)
-
-            checkRange = self.getRangeForDirection(direction)
-            foundPoint = self.findCandidateInHorizontalRange(checkRange, currentPoint)
-
-            if foundPoint == (0,0) and direction !=0:
-                checkRange = self.getRangeForDirection(-direction)
-                foundPoint = self.findCandidateInHorizontalRange(checkRange, currentPoint)
-
-            if foundPoint == (0, 0):
-                break
-
-            direction = self.determineNewHorizontalDirection(currentPoint, foundPoint)
-
-            currentPoint = foundPoint
-            self.stemCore.append(currentPoint)
-
-
-        edgeLeft = []
-        edgeRight = []
-        thickness = []
-        maxThickness = 80
-        for point in self.stemCore:
-            x = point[1]
-
-            leftFoundEdge = (0, 0)
-            edgeCounter = 0
-            rangeStart = np.max([point[0] - maxThickness, 0])
-            for y in range( point[0] - 1, rangeStart, -1):
-                if self.edges[x][y] > 0:
-                    leftFoundEdge = (y, x)
-                    edgeCounter = 0
-                    self.leftEdgeStem.append(leftFoundEdge)
-                    break
-
-
-            rangeEnd = np.min([point[0] + maxThickness, len(self.edges[x])])
-            rightFoundEdge = (0, 0)
-            for y in range(point[0], rangeEnd):
-                if self.edges[x][y] > 0:
-                    rightFoundEdge = (y, x)
-                    self.rightEdgeStem.append(rightFoundEdge)
-                    break
-            if (rightFoundEdge != (0, 0) and leftFoundEdge != (0, 0)):
-                thickness.append(np.abs(rightFoundEdge[0] - leftFoundEdge[0]))
-
-    def findCandidateInHorizontalRange(self, checkRange, currentPoint):
-        foundPoint = (0, 0)
-        for change in checkRange:
-            newPoint = (currentPoint[0] + change, currentPoint[1])
-            if (newPoint[0] > 0 and np.sum(self.img[newPoint[1]][newPoint[0]]) > 0):
-                foundPoint = newPoint
-                break
-        return foundPoint
-
-    def determineNewHorizontalDirection(self, currentPoint, foundPoint):
-        if foundPoint[0] > currentPoint[0]:
-            direction = 1
+    def getCrossingLengthAnalysis(self):
+        stemLength =self.stemCore[len(self.stemCore)-1][1]-self.stemCore[0][1] +0.0
+        labelLength= self.labelCore[len(self.labelCore)-1][0]-self.labelCore[0][0] +0.0
+        ratio = stemLength/labelLength -1.2
+        print ratio
+        if(abs(ratio)< 0.25):
+            return "normal"
         else:
-            direction = -1
-        return direction
+            if ratio>0:
+                return "short"
+            else:
+                return "long"
+
 
     def analyze(self):
         self.preprocessImage()
         self.findTopLabel()
         self.findStem()
 
+        self.drawAll()
+        self.analysis["labelTrend"] = self.getLabelTrend()
+        self.analysis["labelThickness"] = self.getLabelThicknessAnalysis()
+        self.analysis["crossingLength"] = self.getCrossingLengthAnalysis()
+
+    def drawAll(self):
         self.drawPoints(self.topEdgeLabel, Colors.blue)
         self.drawPoints(self.bottomEdgeLabel, Colors.purple)
         self.drawPoints(self.stemCore, Colors.green)
         self.drawPoints(self.rightEdgeStem, Colors.yellow)
         self.drawPoints(self.leftEdgeStem, Colors.orange)
         self.drawPoints(self.labelCore, Colors.red)
-
-        self.analysis["labelTrend"] = self.getLabelTrend()
-
-        self.analysis["labelThickness"] = self.getLabelThicknessAnalysis()
 
     def getAnalysis(self):
         return self.analysis
@@ -287,7 +326,7 @@ class TDataExtractor:
 
 if __name__ == '__main__':
 
-    for i in range(1,21):
+    for i in range(20,21):
         img = cv2.imread('tImages/t' + str(i) + '.png')
 
         extractor = TDataExtractor(img)
--- test_tDataExtractor.py
+++ test_tDataExtractor.py
@@ -9,103 +9,123 @@ class Test_tDataExtractor:
         analysis = self.getAnalysis('tImages/t1.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'short'
 
 
     def test_image2(self):
         analysis = self.getAnalysis('tImages/t2.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'long'
 
     def test_image3(self):
         analysis = self.getAnalysis('tImages/t3.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'long'
 
 
     def test_image4(self):
         analysis = self.getAnalysis('tImages/t4.png')
         assert analysis['labelTrend'] == 'falling'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image5(self):
         analysis = self.getAnalysis('tImages/t5.png')
         assert analysis['labelTrend'] == 'rising'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'long'
 
     def test_image6(self):
         analysis = self.getAnalysis('tImages/t6.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image7(self):
         analysis = self.getAnalysis('tImages/t7.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image8(self):
         analysis = self.getAnalysis('tImages/t8.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image9(self):
         analysis = self.getAnalysis('tImages/t9.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image10(self):
         analysis = self.getAnalysis('tImages/t10.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image11(self):
         analysis = self.getAnalysis('tImages/t11.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image12(self):
         analysis = self.getAnalysis('tImages/t12.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'falling'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image13(self):
         analysis = self.getAnalysis('tImages/t13.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'long'
 
     def test_image14(self):
         analysis = self.getAnalysis('tImages/t14.png')
         assert analysis['labelTrend'] == 'rising'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'normal'
 
     def test_image15(self):
         analysis = self.getAnalysis('tImages/t15.png')
         assert analysis['labelTrend'] == 'falling'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'long'
 
     def test_image16(self):
         analysis = self.getAnalysis('tImages/t16.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'long'
 
     def test_image17(self):
         analysis = self.getAnalysis('tImages/t17.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'constant'
+        assert analysis['crossingLength'] == 'long'
 
     def test_image18(self):
         analysis = self.getAnalysis('tImages/t18.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'rising'
+        assert analysis['crossingLength'] == 'long'
 
     def test_image19(self):
         analysis = self.getAnalysis('tImages/t19.png')
         assert analysis['labelTrend'] == 'vertical'
         assert analysis['labelThickness'] == 'falling'
+        assert analysis['crossingLength'] == 'long'
 
     def test_image20(self):
         analysis = self.getAnalysis('tImages/t20.png')
         assert analysis['labelTrend'] == 'vertical'
-        assert analysis['labelThickness'] == 'constant'
+        assert analysis['labelThickness'] == 'falling'
+        assert analysis['crossingLength'] == 'long'
 
     def getAnalysis(self, imageFile):
         img = cv2.imread(imageFile)
